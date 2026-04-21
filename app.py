4a791b92cf198958ad84eb911c6897892553a493
Kamlesh-Panwar <kpanwars1998@gmail.com>
2026-04-21 11:05:30 +0530

    changes done

--- app.py
+++ app.py
@@ -8,12 +8,11 @@ chatbot = ChatbotModel("dataset.json")
 @app.route('/')
 def home():
     return render_template("index.html")
-
 @app.route('/chat', methods=['POST'])
 def chat():
     user_input = request.json.get('message')
-    response = chatbot.get_response(user_input)
-    return jsonify({"response": response})
+    result = chatbot.get_response(user_input)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
